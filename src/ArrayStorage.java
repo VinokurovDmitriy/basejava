ce1bb38ca7ad4c6a956ea4e6b671492ebc91b7df
VinokurovDmitriy <dnitvitvin@gmail.com>
2022-11-20 13:50:05 +0300

    base java first lesson home work

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,57 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-
+    private int count = 0;
     void clear() {
+        Arrays.fill(storage, 0, count,null);
+        count = 0;
     }
 
     void save(Resume r) {
+        int index = findIndex(r.uuid);
+        if(index < 0) {
+            storage[count] = r;
+            count++;
+        } else {
+            System.out.println("Resume with this uuid already exists");
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int index = findIndex(uuid);
+        return index >= 0 ? storage[index] : null;
     }
 
     void delete(String uuid) {
+        int index = findIndex(uuid);
+        if(storage[index].uuid.equals(uuid)) {
+            System.arraycopy(storage, index + 1, storage, index, count - index - 1);
+            count--;
+            storage[count] = null;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, count);
     }
 
     int size() {
-        return 0;
+        return count;
+    }
+
+    private int findIndex(String uuid) {
+        for(int i = 0; i < count; i++) {
+            if(storage[i].uuid.equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- src/MainArray.java
+++ src/MainArray.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * Interactive test for ArrayStorage implementation
